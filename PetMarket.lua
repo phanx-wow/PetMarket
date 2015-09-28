854012c89367ffc9d2ac9b62705eb38f65eab0f1
Phanx <addons@phanx.net>
2015-09-27 20:52:43 -0700

    Add comment header

--- PetMarket.lua
+++ PetMarket.lua
@@ -1,3 +1,29 @@
+--[[--------------------------------------------------------------------
+	PetMarket
+	Scans auction house for unowned pets.
+	http://www.curse.com/addons/wow/petmarket
+	http://wow.curseforge.com/addons/petmarket/
+
+	Copyright (C) 2014 Timothy Johnson (RedHatter21)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	-----
+	Forked version with fixes by Phanx:
+	https://github.com/Phanx/PetMarket
+----------------------------------------------------------------------]]
+
 PetMarket = LibStub ("AceAddon-3.0"):NewAddon ("PetMarket", "AceEvent-3.0", "AceTimer-3.0")
 
 -- Pet lists
